ac953f95557b85ab80ec7bad8d4ea88b576d4cd1
Inshra23 <164750803+Inshra23@users.noreply.github.com>
2024-05-15 16:13:26 +0500

    learningjavascript

---
+++ script.js
@@ -0,0 +1,91 @@
+//class activity
+//Question no 1
+//  let age = prompt("Enter Your Age")
+// if(age < 18){
+//     console.log("You are eligible to vote");
+// }
+// else if(age > 18){
+//     console.log("you are not eligible to vote");
+// }
+//Question no 2
+//  let temperature = +prompt("Enter temperature in celsius")
+// if(temperature < 25){
+//     console.log("It's a hot day");
+// }
+// else if(temperature > 25){
+//     console.log("It's a normal day");
+// }
+//Question no 3
+//  let student;sscore = +prompt("Enter your score")
+// if(score <= 60){
+//     console.log("You passed the test");
+// }
+// else if(score > 60){
+//     console.log("You failed the test");
+// }
+//Question no 4
+//  let raining = true
+// if(raining == true){
+//     console.log("Remember to bring an umbrella");
+// }
+// else{
+//     console.log("Enjoy the sunny weather!");
+// }
+
+//Question no 5
+// let b = +prompt("enter your age")
+// if(b < 18){
+//     console.log("you are an adult");
+// }
+// else if(b > 18){
+//     console.log("you are minor");
+// }
+//Question no 6
+// let number = +prompt("Enter the number")
+// if(number > 0){
+//     console.log("The number is positive");
+// }
+// else if(number < 0){
+//     console.log("The number is negative");
+// }
+// else if(number == 0){
+//     console.log("The number is zero");
+// }
+
+//chapter 11
+
+// let score = +prompt("Enter your score")
+// if(score >= 80 && score <=60){
+//     console.log("you are passed");
+// }
+// else{
+//     console.log("you are failed");
+// }//logical operator AND &&
+
+// let score = +prompt("Enter your score")
+// if(score >= 80 || score >=60){
+//     console.log("you passed");
+// }
+// else{
+//     console.log("you failed");
+// } // logical operator OR ||
+
+// let isSunny = true;
+// let isWarm = false;
+
+// if(isSunny && isWarm){
+//     console.log("Perfect weather for outdoor activities");
+// }
+// else{
+//     console.log("May be for another day");
+// }
+
+let isRaining = "hello"
+if(!isRaining){
+    console.log("It's not raining. Enjoy your day");
+}
+else{
+    console.log("Don't forget your umbrella");
+} //logical operator ! not
+
+
